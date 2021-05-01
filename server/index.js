fac9347aabb6d6941037bd4b23e76ec0e9833242
Corey Cleary <1485356+coreyc@users.noreply.github.com>
2021-05-01 13:06:44 -0500

    add redismod env

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const IORedis = require(`ioredis`)
+const dotenv = require(`dotenv`)
 const { items } = require(`./auction-items.json`)
 const { addInitialAuctionItems } = require(`./bootstrap`)
 
-const redis = new IORedis()
+dotenv.config()
+
+const redis = new IORedis(`//${process.env.REDIS}:6379`)
 
 await addInitialAuctionItems(items, redis)
